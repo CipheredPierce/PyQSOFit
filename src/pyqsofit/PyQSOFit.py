5b6ca01d2cf1bb58113ceb2ff7d2cfa3326b25c9
Morgan Robbins <mrobbin1@umbc.edu>
2026-06-30 10:32:24 -1000

    testing commit

--- src/pyqsofit/PyQSOFit.py
+++ src/pyqsofit/PyQSOFit.py
@@ -805,7 +805,7 @@ class QSOFit():
         host_spec = datacube[1, :] - datacube[4, :]
 
         print(f"Testing what causes failures")
-        print(f"Host: {(np.sum(np.where(datacube[3, :] < 0, True, False))) > 0.1 * \
+        print(f"Host Update happened: {(np.sum(np.where(datacube[3, :] < 0, True, False))) > 0.1 * \
                 datacube.shape[1]}")
         print(f"QSO: {(np.sum(np.where(datacube[4, :] < 0, True, False))) > 0.1 * \
                 datacube.shape[1]}")
