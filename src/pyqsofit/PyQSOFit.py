ed7463a094c276d66695105b4017d4fb531a2ae7
Morgan Robbins <mrobbin1@umbc.edu>
2026-06-30 10:33:45 -1000

    continue testing update

--- src/pyqsofit/PyQSOFit.py
+++ src/pyqsofit/PyQSOFit.py
@@ -805,10 +805,8 @@ class QSOFit():
         host_spec = datacube[1, :] - datacube[4, :]
 
         print(f"Testing what causes failures")
-        print(f"Host Update happened: {(np.sum(np.where(datacube[3, :] < 0, True, False))) > 0.1 * \
-                datacube.shape[1]}")
-        print(f"QSO: {(np.sum(np.where(datacube[4, :] < 0, True, False))) > 0.1 * \
-                datacube.shape[1]}")
+        print(f"Host Update happened: {(np.sum(np.where(datacube[3, :] < 0, True, False))) > 0.1 * datacube.shape[1]}")
+        print(f"QSO: {(np.sum(np.where(datacube[4, :] < 0, True, False))) > 0.1 * datacube.shape[1]}")
         print(f"Host flux too weak: {np.median(datacube[3, :]) < 0.01 * flux_level}")
         print(f"Host-implied residual mostly negative: {np.median(host_spec) < 0}")
         if np.sum(np.where(datacube[3, :] < 0, True, False) | np.where(datacube[4, :] < 0, True, False)) > 0.1 * \
